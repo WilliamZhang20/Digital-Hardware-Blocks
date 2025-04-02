0a83a4d67c831683bb32a76cd3d605f8532c4485
Bill <wzhang20@yahoo.com>
2025-04-01 20:12:14 -0400

    Add more

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Learning-Verilog
+
+A special collection of practical digital hardware elements in Verilog.
+
+Testbenched using Verilator and C++.
--- adder.v
+++ adder.v
@@ -1,4 +1,4 @@
-// simple_adder.v
+// a VERY simple 8-bit adder...
 module simple_adder(input [7:0] a, input [7:0] b, output [7:0] sum);
 
   assign sum = a + b;

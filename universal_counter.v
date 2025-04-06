1c60904fa57fc90d748a4636989a01545d86f8b7
Bill <wzhang20@yahoo.com>
2025-04-06 13:08:16 -0400

    add counter

---
+++ universal_counter.v
@@ -0,0 +1,22 @@
+// a simple counter that can count both up and down
+module counter (
+    input clk, // clock signal
+    input rst, // reset output signal
+    input up_down, // 1 for counting up, 0 for counting down
+    output reg[7:0] count // 8-bit counter
+);
+
+    // Asynchronously reset
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            count <= 8'b0 // reset all 8 bits to 0
+        end else begin
+            if (up_down) begin
+                count <= count + 1;
+            end else begin
+                count <= count - 1;
+            end
+        end
+    end
+
+endmodule

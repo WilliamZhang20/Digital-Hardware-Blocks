1ee33bcc2d10b404a46fa4b13d5a77c265477120
Bill <wzhang20@yahoo.com>
2025-04-07 21:10:28 -0400

    add mealy machine

--- fibonacci.v
+++ fibonacci.v
@@ -6,6 +6,7 @@ module fibonacci (
 );
 
     reg [3:0] fib_prev, fib_curr;
+    reg [4:0] sum; // extra bit to detect overflow
 
     always @(posedge clk or posedge reset) begin
         if (reset) begin
@@ -14,10 +15,18 @@ module fibonacci (
             fib <= 4'b0000;      // Output initially 0
         end
         else begin // non-blocking assignments used 
-            // therefore, must read the old value and THEN update forward
-            fib <= fib_curr; // Output current value
-            fib_curr <= (fib_prev + fib_curr) & 4'hF;
-            fib_prev <= fib_curr;
+            fib <= fib_curr;
+            sum = fib_prev + fib_curr; // full 5-bit addition
+            if (sum >= 5'd16) begin
+                // 4-bit overflow detected: reset sequence
+                fib_prev <= 4'd0;
+                fib_curr <= 4'd1;
+            end
+            else begin
+                // Normal Fibonacci update
+                fib_prev <= fib_curr;
+                fib_curr <= sum[3:0];
+            end
         end
     end
 
---
+++ mealy_machine.v
@@ -0,0 +1,59 @@
+// A template Mealy Machine for parity checking
+// Runs 1 clock cycle ahead of Moore
+module mealy_machine (
+    input wire clk,
+    input wire rst,
+    input wire din,
+    output reg dout
+);
+
+    typedef enum logic [1:0] {
+        S1 = 2'b00,
+        S2 = 2'b01
+    } state_t;
+
+    state_t state, next_state;
+
+    always_ff @(posedge clk or posedge rst) begin
+        if(rst)
+            state <= S1; // start with even (zero 1's is even)
+        else
+            state <= next_state;
+    end
+
+    // forcing always combinational logic (gates or muxes only)
+    // avoids latch inference
+    always_comb begin
+        next_state = state;
+        dout = 0;
+
+        case (state)
+            S1: begin
+                // case on inputs
+                if (din) begin
+                    next_state = S2;
+                end 
+                else begin
+                    next_state = S1;
+                    dout = 1;
+                end
+            end
+
+            S2 : begin
+                if (din) begin
+                    next_state = S1;
+                    dout = 1;
+                end else begin
+                    next_state = S2;
+                    dout = 0;
+                end
+            end
+        
+            default: begin
+                next_state = S1;
+                dout = 1;
+            end
+        endcase
+    end
+
+endmodule
